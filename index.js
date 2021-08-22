2e1b987f2b861dc34c3b6fad619ce4b1548211c6
Roger Mendez <roger61087@gmail.com>
2021-08-22 09:43:09 -0400

    commit tree 3cdfcda60317d9eef68eebcb2f2f05696a219b4a

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 32;
+let num4 = 8;
+const mod = num3 / num4
+
+let max = Math.max(1,3,4,5,20);
